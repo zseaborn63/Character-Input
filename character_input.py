e2afda7c311aa34e387b788e79150e7ef67375d0
zseaborn63 <zseaborn63@gmail.com>
2016-02-17 13:46:36 -0500

    Basic program working

--- character_input.py
+++ character_input.py
@@ -1,1 +1,9 @@
-print("Hello World")
+
+
+
+print("Hello!\n Welcome to the Age Teller!")
+name = input("What is your name?\n")
+age = input("And how old are you today?\n")
+year = 2016
+year_100 = (100 - int(age)) + (year - 1)
+print("{}, you will turn 100 in the year {}!".format(name, year_100))
